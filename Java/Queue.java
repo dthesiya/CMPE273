0d2c087e5a7b1a0e45e436483353bc1d597c7711
Darshit <darshit.thesiya@sjsu.edu>
2016-09-14 22:57:07 -0700

    Changes in Queue

--- Java/Queue.java
+++ Java/Queue.java
@@ -1,9 +1,7 @@
-import java.util.*;
+interface QueueInterface {
+	void enQueue(Object obj) throws QueueFullException;
 
-interface QueueInterface<T> {
-	void enQueue(T t) throws QueueFullException;
-
-	T deQueue() throws QueueEmptyException;
+	Object deQueue() throws QueueEmptyException;
 
 	boolean isEmpty();
 
@@ -12,35 +10,40 @@ interface QueueInterface<T> {
 	int getSize();
 }
 
-public class Queue<T> implements QueueInterface {
-	List<T> queue;
-	int size = 0;
+public class Queue implements QueueInterface {
+	Object[] queue;
+	int size = 0, back = 0;
 
 	public Queue(int size) {
 		this.size = size;
-		queue = new ArrayList<T>();
+		queue = new Object[size];
 	}
 
-	public void enQueue(Object t) throws QueueFullException {
-		if (queue.size() == size) {
+	public void enQueue(Object obj) throws QueueFullException {
+		if (back == size) {
 			throw new QueueFullException();
 		}
-		queue.add((T) t);
+		queue[back++] = obj;
 	}
 
-	public T deQueue() throws QueueEmptyException {
-		if (queue.size() == 0) {
+	public Object deQueue() throws QueueEmptyException {
+		if (back == 0) {
 			throw new QueueEmptyException();
 		}
-		return queue.remove(0);
+		Object result = queue[0];
+		for (int i = 0; i < back - 1; i++) {
+			queue[i] = queue[i + 1];
+		}
+		back--;
+		return result;
 	}
 
 	public boolean isEmpty() {
-		return queue.size() == 0;
+		return back == 0;
 	}
 
 	public boolean isFull() {
-		return queue.size() == size;
+		return back == size;
 	}
 
 	public int getSize() {
@@ -48,33 +51,33 @@ public class Queue<T> implements QueueInterface {
 	}
 
 	public static void main(String[] args) {
-		Queue<String> queue=new Queue<>(5);
-//		try {
-			queue.enQueue("1");
-			queue.enQueue("2");
-			queue.enQueue("3");
-			queue.enQueue("4");
-			queue.enQueue("5");
-			System.out.println(queue.isFull());
-			System.out.println(queue.deQueue());
-			System.out.println(queue.deQueue());
-			System.out.println(queue.deQueue());
-			System.out.println(queue.deQueue());
-			System.out.println(queue.deQueue());
-			System.out.println(queue.isEmpty());
-			System.out.println(queue.deQueue());
-//		} catch (QueueFullException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}catch (QueueEmptyException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		Queue queue = new Queue(5);
+		// try {
+		queue.enQueue("1");
+		queue.enQueue("2");
+		queue.enQueue("3");
+		queue.enQueue("4");
+		queue.enQueue("5");
+		System.out.println(queue.isFull());
+		System.out.println(queue.deQueue());
+		System.out.println(queue.deQueue());
+		System.out.println(queue.deQueue());
+		System.out.println(queue.deQueue());
+		System.out.println(queue.deQueue());
+		System.out.println(queue.isEmpty());
+		System.out.println(queue.deQueue());
+		// } catch (QueueFullException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }catch (QueueEmptyException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
 	}
 
 }
 
-class QueueFullException extends RuntimeException{
+class QueueFullException extends RuntimeException {
 	public String toString() {
 		return "Cannot add element since Queue is full";
 	}
